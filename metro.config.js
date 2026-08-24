4d8ffc5ee372de78ae9d3841941386a82ef8a9d7
learnersthings <redquantumlabs@gmail.com>
2026-08-24 11:17:39 +0530

    Dev

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,12 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    unstable_enablePackageExports: true,
+    unstable_conditionNames: ['require', 'react-native'],
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
